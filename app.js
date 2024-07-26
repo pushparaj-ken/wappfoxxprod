2f80cc7f31c5fcb960ba7c4d36bf4b48af85a7d4
pushparaj-ken <79186935+pushparaj-ken@users.noreply.github.com>
2024-07-26 17:25:08 +0530

    cors chnages

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://demo1.smcreativemedia.in', 'http://consense.wappfoxx.de']
+  origin: ['http://localhost:3000', 'https://demo1.smcreativemedia.in', 'https://consense.wappfoxx.de', 'http://consense.wappfoxx.de']
 }));
 
 app.use(logger('dev'));
